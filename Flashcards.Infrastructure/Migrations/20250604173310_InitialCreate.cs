0a317cd3345063557bb633fbc244be7f5b7afb28
NiklasRoos91 <niklas_roos@live.se>
2025-06-04 19:41:10 +0200

    Created the DBContext and made migration to the databse

---
+++ Flashcards.Infrastructure/Migrations/20250604173310_InitialCreate.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Flashcards.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlashcardLists",
+                columns: table => new
+                {
+                    FlashcardListId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlashcardLists", x => x.FlashcardListId);
+                    table.ForeignKey(
+                        name: "FK_FlashcardLists_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flashcards",
+                columns: table => new
+                {
+                    FlashcardId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FlashcardListId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flashcards", x => x.FlashcardId);
+                    table.ForeignKey(
+                        name: "FK_Flashcards_FlashcardLists_FlashcardListId",
+                        column: x => x.FlashcardListId,
+                        principalTable: "FlashcardLists",
+                        principalColumn: "FlashcardListId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlashcardLists_UserId",
+                table: "FlashcardLists",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flashcards_FlashcardListId",
+                table: "Flashcards",
+                column: "FlashcardListId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flashcards");
+
+            migrationBuilder.DropTable(
+                name: "FlashcardLists");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Flashcards.Infrastructure/Presistence/FlashcardsDbContext/FlashcardsDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Flashcards.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flashcards.Infrastructure.Presistence.FlashcardsDbContext
+{
+    public class FlashcardsDbContext : DbContext
+    {
+        public FlashcardsDbContext(DbContextOptions<FlashcardsDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<FlashcardList> FlashcardLists { get; set; }
+
+        public DbSet<Flashcard> Flashcards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
